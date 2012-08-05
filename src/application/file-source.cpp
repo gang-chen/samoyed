e2081403a7c498089a126cc49c59cca283224c75
gang-chen <gang.chen.cn@gmail.com>
2012-08-05 17:26:28 +0800

    Update src/application/file-source.cpp

--- src/application/file-source.cpp
+++ src/application/file-source.cpp
@@ -32,19 +32,12 @@ void FileSource::Loading::onLoaded(Worker &worker)
     if (src)
     {
         TextFileReader &reader = static_cast<TextFileReader &>(worker);
-        Revision oldRev;
-        GError *oldError;
-        src->beginWrite(false, NULL, &oldRev, &oldError);
-        if (oldRev != reader.revision() || oldError)
-        {
-            TextBuffer *buffer = reader.fetchBuffer();
-            src->setBuffer(buffer);
-            src->endWrite(reader.revision(),
-                          reader.fetchError(),
-                          Range(0, buffer->length()));
-        }
-        else
-            src->endWrite(oldRev, oldError, Range());
+        src->beginWrite(false, NULL, NULL, NULL);
+        TextBuffer *buffer = reader.fetchBuffer();
+        src->setBuffer(buffer);
+        src->endWrite(reader.revision(),
+                      reader.fetchError(),
+                      Range(0, buffer->length()));
         src->onChangeDone(this);
     }
     delete &worker;
@@ -61,70 +54,53 @@ bool FileSource::RevisionChange::execute(FileSource &source)
 bool FileSource::Insertion::execute(FileSource &source)
 {
     TextBuffer *buffer;
-    Revision oldRev;
     GError *oldError;
-    if (!source.beginWrite(true, &buffer, &oldRev, &oldError))
+    if (!source.beginWrite(true, &buffer, NULL, &oldError))
         return false;
-    if (oldRev == m_revision)
-        source.endWrite(oldRev, oldError, Range());
-    else
-    {
-        buffer->setLineColumn(m_line, m_column);
-        buffer->insert(m_text.c_str(), m_text.length(), -1, -1);
-        source.endWrite(m_revision,
-                        oldError,
-                        Range(buffer->cursor() - m_text.length(),
-                              buffer->cursor()));
-    }
+    buffer->setLineColumn(m_line, m_column);
+    buffer->insert(m_text.c_str(), m_text.length(), -1, -1);
+    source.endWrite(m_revision,
+                    oldError,
+                    Range(buffer->cursor() - m_text.length(),
+                          buffer->cursor()));
     return true;
 }
 
-bool SourceImage::Removal::execute(SourceImage &source)
+bool FileSource::Removal::execute(FileSource &source)
 {
     TextBuffer *buffer;
-    Revision oldRev;
-    if (!source.beginWrite(true, &buffer, &rev, NULL))
+    GError *oldError;
+    if (!source.beginWrite(true, &buffer, NULL, &oldError))
         return false;
-    if (rev == m_revision)
-        source.endWrite(m_revision, NULL, Range());
-    else
-    {
-        buffer->setCursor2(m_charIndex);
-        int endIndex, endIndex2;
-        endIndex = -1;
-        endIndex2 = m_charIndex + m_numChars;
-        buffer->convertIndex(&endIndex, &endIndex, NULL);
-        buffer->remove(endIndex - buffer->cursor(), m_numChars, -1);
-        source.endWrite(m_revision,
-                        NULL,
-                        Range(buffer->cursor(), buffer->cursor()));
-    }
+    buffer->setLineColumn(m_beginLine, m_beginColumn);
+    int endByteOffset;
+    buffer->transformLineColumnToByteOffset(m_endLine,
+                                            m_endColumn,
+                                            endByteOffset);
+    buffer->remove(endIndex - buffer->cursor(), -1, -1);
+    source.endWrite(m_revision,
+                    oldError,
+                    Range(buffer->cursor()));
     return true;
 }
 
-bool SourceImage::Replacement::execute(SourceImage &source)
+bool FileSource::Replacement::execute(FileSource &source)
 {
     TextBuffer *buffer;
-    Revision rev;
-    if (!source.beginWrite(true, &buffer, &rev, NULL))
+    if (!source.beginWrite(true, &buffer, NULL, NULL))
         return false;
-    if (rev == m_revision)
-        source.endWrite(m_revision, NULL, Range());
-    else
-    {
-        buffer->removeAll();
-        buffer->insert(m_text, strlen(m_text), -1, -1);
-        source.endWrite(m_revision,
-                        NULL,
-                        Range(0, buffer->length()));
-    }
+    buffer->removeAll();
+    buffer->insert(m_text, strlen(m_text), -1, -1);
+    source.endWrite(m_revision,
+                    m_error,
+                    Range(0, buffer->length()));
     return true;
 }
 
-SourceImage::SourceImage(const std::string &fileName,
-                         unsigned long serialNumber,
-                         Manager<SourceImage> &mgr):
-    Managed<std::string, SourceImage>(fileName, serialNumber, mgr),
+FileSource::FileSource(const std::string &uri,
+                       unsigned long serialNumber,
+                       Manager<FileSource> &mgr):
+    Managed<std::string, FileSource>(uri, serialNumber, mgr),
     m_error(NULL),
     m_executingChange(false)
 {
@@ -132,18 +108,18 @@ SourceImage::SourceImage(const std::string &fileName,
     synchronize();
 }
 
-SourceImage::~SourceImage()
+FileSource::~FileSource()
 {
-    assert(!m_loader);
     delete m_buffer;
-    g_error_free(m_error);
+    if (m_error)
+        g_error_free(m_error);
     clearPendingChanges();
 }
 
-bool SourceImage::beginRead(bool trying,
-                            const TextBuffer **buffer,
-                            Revision *revision,
-                            GError **error) const
+bool FileSource::beginRead(bool trying,
+                           const TextBuffer **buffer,
+                           Revision *revision,
+                           GError **error) const
 {
     if (trying)
     {
@@ -157,11 +133,11 @@ bool SourceImage::beginRead(bool trying,
     if (revision)
         *revision = m_revision;
     if (error)
-        *error = g_error_copy(m_error);
+        *error = m_error;
     return true;
 }
 
-void SourceImage::endRead()
+void FileSource::endRead()
 {
     m_dataMutex.unlock_shared();
 
@@ -170,10 +146,10 @@ void SourceImage::endRead()
     executePendingChanges();
 }
 
-bool SourceImage::beginWrite(bool trying,
-                             TextBuffer **buffer,
-                             Revision *revision,
-                             GError **error)
+bool FileSource::beginWrite(bool trying,
+                            TextBuffer **buffer,
+                            Revision *revision,
+                            GError **error)
 {
     if (trying)
     {
@@ -195,13 +171,13 @@ bool SourceImage::beginWrite(bool trying,
     if (revision)
         *revision = m_revision;
     if (error)
-        *error = g_error_copy(m_error);
+        *error = m_error;
     return true;
 }
 
-void SourceImage::endWrite(Revision &revision,
-                           GError *error,
-                           const Range &range)
+void FileSource::endWrite(Revision &revision,
+                          GError *error,
+                          const Range &range)
 {
     Revision oldRev = m_revision;
     GError *oldError = m_error;
@@ -213,34 +189,34 @@ void SourceImage::endWrite(Revision &revision,
         g_error_free(oldError);
     m_error = error;
     m_dataMutex.unlock();
-    if (oldRev != revision || oldError != m_error)
+    if (oldRev != revision || oldError != m_error || !range.empty())
     {
         ChangeHint changeHint(oldRev, revision, range);
         if (revision.synchronized())
         {
-            Revision compRev = Application::instance()->compiledImageManager()->
-                lookupRevision(m_fileName.c_str());
-            if (compRev != revision)
+            Revision astRev = Application::instance()->fileAstManager()->
+                lookupRevision(m_uri.c_str());
+            if (astRev != revision)
             {
-                ReferencePointer<CompiledFile> comp =
-                    Application::instance()->compiledFileManager()->
-                    get(m_fileName.c_str());
-                comp->onSourceChanged(*this, changeHint);
+                ReferencePointer<FileAst> ast =
+                    Application::instance()->fileAstManager()->
+                    get(m_uri.c_str());
+                ast->onSourceChanged(*this, changeHint);
             }
         }
         else
         {
-            ReferencePointer<CompiledImage> comp =
-                Application::instance()->compiledFileManager()->
-                get(m_fileName.c_str());
-            comp->onSourceChanged(*this, changeHint);
+            ReferencePointer<FileAst> ast =
+                Application::instance()->fileAstManager()->
+                get(m_uri.c_str());
+            ast->onSourceChanged(*this, changeHint);
         }
         m_changeSignal(*this, changeHint);
     }
     m_controlMutex.unlock();
 }
 
-void SourceImage::clearPendingChanges()
+void FileSource::clearPendingChanges()
 {
     for (std::deque<Change *>::const_iterator i = m_pendingChangeQueue.begin();
          i != m_pendingChangeQueue.end();
@@ -249,7 +225,7 @@ void SourceImage::clearPendingChanges()
     m_pendingChangeQueue.clear();
 }
 
-void SourceImage::executePendingChanges()
+void FileSource::executePendingChanges()
 {
     {
         boost::mutex::scoped_lock lock(m_pendingChangeMutex);
@@ -286,7 +262,7 @@ void SourceImage::executePendingChanges()
     change->execute(*this);
 }
 
-void SourceImage::onChangeDone(Change *change)
+void FileSource::onChangeDone(Change *change)
 {
     delete change;
     {
@@ -297,7 +273,7 @@ void SourceImage::onChangeDone(Change *change)
     executePendingChanges();
 }
 
-void SourceImage::requestChange(Change *change)
+void FileSource::requestChange(Change *change)
 {
     {
         boost::mutex::scoped_lock lock(m_pendingChangeMutex);
@@ -308,7 +284,7 @@ void SourceImage::requestChange(Change *change)
     executePendingChanges();
 }
 
-void SourceImage::onDocumentClosed(const Document &document)
+void FileSource::onDocumentClosed(const Document &document)
 {
     if (document.editCount())
     {
@@ -319,34 +295,37 @@ void SourceImage::onDocumentClosed(const Document &document)
     }
 }
 
-void SourceImage::onDocumentLoaded(const Document &document)
+void FileSource::onDocumentLoaded(const Document &document)
 {
-    Replacement *rep = new Replacement(document.revision(), document.getText());
+    Replacement *rep = new Replacement(document.revision(),
+                                       document.ioError(),
+                                       document.getText());
     requestChange(rep);
 }
 
-void SourceImage::onDocumentSaved(const Document &document)
+void FileSource::onDocumentSaved(const Document &document)
 {
-    RevisionChange *revChange = new RevisionChange(document.revision());
+    RevisionChange *revChange = new RevisionChange(document.revision(),
+                                                   document.ioError());
     requestChange(revChange);
 }
 
-void SourceImage::onDocumentTextInserted(const Document &document,
-                                         int line,
-                                         int column,
-                                         const char *text,
-                                         int length)
+void FileSource::onDocumentTextInserted(const Document &document,
+                                        int line,
+                                        int column,
+                                        const char *text,
+                                        int length)
 {
     Insertion *ins =
         new Insertion(document.revision(), line, column, text, length);
     requestChange(ins);
 }
 
-void SourceImage::onDocumentTextRemoved(const Document &document,
-                                        int beginLine,
-                                        int beginColumn,
-                                        int endLine,
-                                        int endColumn)
+void FileSource::onDocumentTextRemoved(const Document &document,
+                                       int beginLine,
+                                       int beginColumn,
+                                       int endLine,
+                                       int endColumn)
 {
     Removal *rem = new Removal(document.revision(),
                                beginLine,
