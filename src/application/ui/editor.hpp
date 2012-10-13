0484a29262d127627422baa9e0b4b16c6ef2ed1d
gang.chen.cn@gmail.com <gangchen@t61>
2012-10-13 23:31:39 +0800

    change file names

--- src/application/ui/editor.hpp
+++ src/application/ui/editor.hpp
@@ -1,13 +1,15 @@
-// Editor.
+// File editor.
 // Copyright (C) 2012 Gang Chen.
 
-#ifndef SMYD_EDITOR_HPP
-#define SMYD_EDITOR_HPP
+#ifndef SMYD_FILE_EDITOR_HPP
+#define SMYD_FILE_EDITOR_HPP
+
+#include "editor.hpp"
 
 namespace Samoyed
 {
 
-class Editor
+class FileEditor: public Editor
 {
 };
 
