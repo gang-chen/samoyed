c62fe5abccea57cfc34467f415c712a4488a60aa
gang-chen <gang.chen.cn@gmail.com>
2012-11-07 10:09:00 +0800

    Update src/application/session.hpp

--- src/application/session.hpp
+++ src/application/session.hpp
@@ -17,27 +17,28 @@ namespace Samoyed
  * A session is locked when it is active in an instance of the application.  A
  * lock file is created when a session starts.  The lock file is removed when
  * the session quits.  The lock file records the process ID.  The lock file may
- * exist if the session quits abnormally.
+ * exist after the session quits abnormally.
  *
- * A session can also be recovered from an abnormal process exit.
- * The URIs of all edited and unsaved files in a session are recorded and all performed
- * edits are saved in replay files.  When the session is
- * restored, these files can be recovered by replaying the edits saved in the replay files.
+ * A session can also be recovered from an abnormal process exit.  The URIs of
+ * all edited and unsaved files in a session are recorded and all performed
+ * edits are saved in replay files.  When the session is restored, these files
+ * can be recovered by replaying the edits saved in the replay files.
+                                                                                
  *
  * The XML file, the lock file and the file recording the unsaved files for a
  * session are in a directory whose name is the session name.
  *
- * When a session is active, the lock file and the replay files exist.  If
- * the session quits normally, the lock file and the replay files will be
- * removed.  If the session quits abnormally, the replay files will be
- * kept, and the lock file will be left.  When the lock file exists, we need to
- * further check to see if the locking process exiIts or not, and if not, the
- * lock file should be discarded.
+ * When a session is active, the lock file and the replay files exist.  If the
+ * session quits normally, the lock file and the replay files will be removed.
+ * If the session quits abnormally, the replay files will be kept, and the lock
+ * file will be left.  When the lock file exists, we need to further check to
+ * see if the locking process exits or not, and if not, the lock file should be
+ * discarded.
  */
 class Session
 {
 public:
-    static bool ensureSessionsDirectoryExists();
+    static bool makeSessionsDirectory();
 
     static const char *lastSessionName();
 
@@ -56,13 +57,10 @@ public:
      */
     bool restore();
 
-    bool save();
-
     /**
-     * Save and quit this session.  Close all opened files and projects.  This will eventually terminate the instance of the application.
-     * @return False iff the user requests not to quit this session.
+     * Save this session.
      */
-    bool quit();
+    bool save();
 
     void addUnsavedFileUri(const char *uri);
     void removeUnsavedFileUri(const char *uri);
