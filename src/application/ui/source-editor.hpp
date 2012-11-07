25b5202275ac50278885c85a6e484218a4972653
gang-chen <gang.chen.cn@gmail.com>
2012-11-07 13:09:57 +0800

    Update src/application/ui/source-editor.hpp

--- src/application/ui/source-editor.hpp
+++ src/application/ui/source-editor.hpp
@@ -17,19 +17,22 @@ class SourceEditor: public Editor
 {
 public:
     /**
-     * Create the data that will be shared by all the documents.  It is required
-     * that this function be called before any document is created.  This
-     * function creates a tag group.
+     * Create the data that will be shared by all source editors.  It is
+     * required that this function be called before any source editor is
+     * created.  This function creates a tag group.
      */
     static void createSharedData();
 
     static void destroySharedData();
 
 private:
+    /**
+     * The tag table shared by all the GTK+ text buffers.
+     */
+    static GtkTextTagTable *s_sharedTagTable;
+
     GtkTextBuffer *m_buffer;
     GtkTextView *m_view;
-
-    ReferencePointer<FileAst> m_ast;
 };
 
 }
