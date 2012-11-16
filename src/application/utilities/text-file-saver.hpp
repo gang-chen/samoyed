8987dd8e24a93fd53867e68a716df532c1434709
gang-chen <gang.chen.cn@gmail.com>
2012-11-16 23:29:01 +0800

    Update src/application/utilities/text-file-saver.hpp

--- src/application/utilities/text-file-saver.hpp
+++ src/application/utilities/text-file-saver.hpp
@@ -1,78 +1,34 @@
-// Text file writer and worker.
+// Text file saver.
 // Copyright (C) 2012 Gang Chen.
 
-#ifndef SMYD_TEXT_FILE_WRITER_HPP
-#define SMYD_TEXT_FILE_WRITER_HPP
+#ifndef SMYD_TEXT_FILE_SAVER_HPP
+#define SMYD_TEXT_FILE_SAVER_HPP
 
-#include "revision.hpp"
-#include "worker.hpp"
+#include "file-saver.hpp"
 #include <string>
-#include <glib.h>
-#include <gio/gio.h>
 
 namespace Samoyed
 {
 
-class TextBuffer;
-
 /**
- * A text file writer converts text into the external character encoding and
+ * A text file saver converts text into the external character encoding and
  * writes it to a file.
  */
-class TextFileWriter
+class TextFileSaver: public FileSaver
 {
 public:
-    TextFileWriter();
-
-    ~TextFileWriter();
-
-    void open(const char *uri);
-
-    void write(const char *text);
-
-    void close();
-
-    const Revision &revision() const
-    { return m_revision; }
-
-    GError *fetchError()
-    {
-        GError *error = m_error;
-        m_error = NULL;
-        return error;
-    }
-
-private:
-    Revision m_revision;
-
-    GError *m_error;
-};
-
-/**
- * Worker wrapping a text file writer.
- */
-class TextFileWriteWorker: public Worker
-{
-public:
-    TextFileWriteWorker(unsigned int priority,
-                        const Callback &callback,
-                        const char *uri,
-                        const char *text):
-        Worker(priority, callback),
-        m_uri(uri),
+    TextFileSaver(unsigned int priority,
+                  const Callback &callback,
+                  const char *uri,
+                  const char *text):
+        FileSaver(priority, callback, uri),
         m_text(text)
     {}
-
-    TextFileWriter &writer() { return m_writer; }
 
     virtual bool step();
     
 private:
-    std::string m_uri;
-
     std::string m_text;
-
-    TextFileWriter m_writer;
 };
 
 }
