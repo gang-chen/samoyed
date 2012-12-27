926e090a7c5e80a483ae039e65ec2cbfcf10d67c
gang-chen <gang.chen.cn@gmail.com>
2012-12-27 08:38:36 +0800

    Update src/application/ui/pane-base.cpp

--- src/application/ui/pane-base.cpp
+++ src/application/ui/pane-base.cpp
@@ -2,20 +2,8 @@
 // Copyright (C) 2012 Gang Chen.
 
 #include "pane-base.hpp"
-#include "window.hpp"
-#include "split-pane.hpp"
-#include <assert.h>
 
 namespae Samoyed
 {
-
-PaneBase::~PaneBase()
-{
-    assert((window() && !parent()) || (!window() && parent()));
-    if (window())
-        window()->onContentClosed();
-    else if (parent())
-        parent()->onChildClosed(this);
-}
 
 }
