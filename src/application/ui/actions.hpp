3e0326887432ef0856ae3bb6235f1b006510a89e
gang-chen <gang.chen.cn@gmail.com>
2012-12-28 17:46:09 +0800

    Update src/application/ui/actions.hpp

--- src/application/ui/actions.hpp
+++ src/application/ui/actions.hpp
@@ -23,35 +23,47 @@ public:
     static const GtkActionEntry s_basicActionEntries[] =
     {
         // Top-level.
-        { "session", 0, N_("_Session") },
-        { "project", 0, N_("_Project") },
-        { "edit", 0, N_("_Edit") },
-        { "view", 0, N_("_View") },
-        { "help", 0, N_("_Help") },
+        { "session", NULL, N_("_Session") },
+        { "project", NULL, N_("_Project") },
+        { "edit", NULL, N_("_Edit") },
+        { "view", NULL, N_("_View") },
+        { "help", NULL, N_("_Help") },
 
         // Session menu.
-        { "session-new", 0, N_("_New..."), 0,
+        { "session-new", NULL, N_("_New..."), NULL,
           N_("Start a new session"), G_CALLBACK(newSession) },
-        { "session-switch", 0, N_("_Switch..."), 0,
+        { "session-switch", NULL, N_("_Switch..."), NULL,
           N_("Switch to a saved session"), G_CALLBACK(switchSession) },
-        { "session-manage", 0, N_("_Manage..."), 0,
+        { "session-manage", NULL, N_("_Manage..."), NULL,
           N_("Manage saved sessions"), G_CALLBACK(manageSessions) },
-        { "session-quit", 0, N_("_Quit"), "<control>q",
+        { "session-quit", NULL, N_("_Quit"), "<control>q",
           N_("Quit the current session"), G_CALLBACK(quitSession) },
 
         // Project menu.
-        { "project-new", 0, N_("_New..."), 0,
-          N_("Create a new project"), G_CALLBACK(newProject) },
-        { "project-open", 0, N_("_Open..."), 0,
+        { "project-new", NULL, N_("_New..."), NULL,
+          N_("Create a project"), G_CALLBACK(newProject) },
+        { "project-open", NULL, N_("_Open..."), NULL,
           N_("Open a project"), G_CALLBACK(openProject) },
 
+        // Edit menu.
+        { "edit-preferences", NULL, N_("_Preferences..."), NULL,
+          N_("Edit preferences"), G_CALLBACK(editPreferences) },
+
+        // View menu.
+        { "view-new-window", NULL, N_("New _window"), NULL,
+          N_("Create a window"), G_CALLBACK(newWindow) },
+        { "view-new-subwindow", NULL, N_("New _subwindow"), NULL,
+          N_("Create a subwindow"), G_CALLBACK(newSubwindow) },
+        { "view-full-screen", NULL, N_("_Full screen"), NULL,
+          N_("Enter full screen mode"), G_CALLBACK(enterFullScreen) },
+
         // Help menu.
-        { "help-manual", 0, N_("_Manual"), "F1",
+        { "help-manual", NULL, N_("_Manual"), "F1",
           N_("Show the user manual"), G_CALLBACK(showManual) },
-        { "help-tutorial", 0, N_("_Tutorial"), 0,
+        { "help-tutorial", NULL, N_("_Tutorial"), NULL,
           N_("Show the tutorial"), G_CALLBACK(showTutorial) },
-        { "help-about", 0, N_("_About"), 0,
-          N_("About this application", G_CALLBACK(showAbout) }
+        { "help-about", NULL, N_("_About"), NULL,
+          N_("About Samoyed", G_CALLBACK(showAbout) }
     };
 
     /**
@@ -59,6 +71,7 @@ public:
      */
     static const GtkActionEntry* s_actionEntriesForProjects[] =
     {
+        // File menu.
     };
 
     /**
@@ -69,13 +82,23 @@ public:
     };
 
 private:
-    static void newSession(GtkAction *, Window *);
+    static void newSession(GtkAction *action, Window *window);
+    static void switchSession(GtkAction *action, Window *window);
+    static void manageSessions(GtkAction *action, Window *window);
+    static void quitSession(GtkAction *action, Window *window);
 
-    static void switchSession(GtkAction *, Window *);
+    static void newProject(GtkAction *action, Window *window);
+    static void openProject(GtkAction *action, Window *window);
 
-    static void manageSessions(GtkAction *, Window *);
+    static void editPreferences(GtkAction *action, Window *window);
 
-    static void quitSession(GtkAction *, Window *);
+    static void newWindow(GtkAction *action, Window *window);
+    static void newSubwindow(GtkAction *action, Window *window);
+    static void enterFullScreen(GtkAction *action, Window *window);
+
+    static void showManual(GtkAction *action, Window *window);
+    static void showTutorial(GtkAction *action, Window *window);
+    static void showAbout(GtkAction *action, Window *window);
 };
 
 }
