74b319f629e3111300da2d773cefb709c3a38c4e
gang-chen <gang.chen.cn@gmail.com>
2013-01-18 13:23:12 +0800

    Update src/application/ui/dialogs/session-chooser-dialog.hpp

--- src/application/ui/dialogs/session-chooser-dialog.hpp
+++ src/application/ui/dialogs/session-chooser-dialog.hpp
@@ -21,9 +21,10 @@ public:
         ACTION_RESTORE
     };
 
-    SessionChooserDialog(Action action, GtkWindow *parent);
-
-    ~SessionChooserDialog();
+    SessionChooserDialog(Action action, GtkWindow *parent):
+        m_action(action),
+        m_parent(parent)
+    {}
 
     void run();
 
@@ -32,58 +33,11 @@ public:
     const char *sessionName() const { return m_sessionName.c_str(); }
 
 private:
-    enum Response
-    {
-        RESPONSE_NEW_SESSION = 1,
-        RESPONSE_RESTORE_SESSION,
-    };
-
-    class Dialog: public boost::noncopyable
-    {
-    public:
-        virtual ~Dialog() {}
-        virtual int run() = 0;
-        virtual const char *name() const = 0;
-    };
-
-    class NewSessionDialog: public Dialog
-    {
-    public:
-        NewSessionDialog(GtkWindow *parent);
-        virtual ~NewSessionDialog();
-        virtual int run();
-        virtual const char *name() const;
-
-    private:
-        static void onResponse(GtkDialog *gtkDialog,
-                               gint response,
-                               gpointer dialog);
-        GtkWidget *m_name;
-        GtkWidget *m_dialog;
-    };
-
-    class RestoreSessionDialog: public Dialog
-    {
-    public:
-        RestoreSessionDialog(GtkWindow *parent);
-        virtual ~RestoreSessionDialog();
-        virtual int run();
-        virtual const char *name() const;
-
-    private:
-        static void onResponse(GtkDialog *gtkDialog,
-                               gint response,
-                               gpointer dialog);
-        GtkListStore *m_store;
-        GtkWidget *m_list;
-        GtkWidget *m_dialog;
-    }
-
-    Dialog *m_dialog;
-
     Action m_action;
 
     std::string m_sessionName;
+
+    GtkWindow *m_parent;
 };
 
 }
