db99186da5d51f2f89d8a849e5eca6917feaee56
gang-chen <gang.chen.cn@gmail.com>
2013-02-19 13:54:19 +0800

    Update src/application/ui/navigation-pane.hpp

--- src/application/ui/navigation-pane.hpp
+++ src/application/ui/navigation-pane.hpp
@@ -4,12 +4,19 @@
 #ifndef SMYD_NAVIGATION_PANE_HPP
 #define SMYD_NAVIGATION_PANE_HPP
 
+#include "notebook.hpp"
+
 namespace Samoyed
 {
 
-class ProjectExplorer;
-class FileExplorer;
-class Symbol;
+/**
+ * A navigation pane is a notebook that contains widgets listing many items and
+ * allowing the user to navigate to different items.  It is usually on the left
+ * side of a window.
+ */
+class NavigationPane: public Notebook
+{
+};
 
 }
 
