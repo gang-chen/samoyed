9e3ed5d727ebac54fd353ef996dd0942e6f5f70a
gang-chen <gang.chen.cn@gmail.com>
2013-02-18 21:23:05 -0800

    Create tools-pane.hpp

---
+++ src/application/ui/tools-pane.hpp
@@ -0,0 +1,22 @@
+// Tools pane.
+// Copyright (C) 2013 Gang Chen.
+
+#ifndef SMYD_TOOLS_PANE_HPP
+#define SMYD_TOOLS_PANE_HPP
+
+#include "notebook.hpp"
+
+namespace Samoyed
+{
+
+/**
+ * A tools pane is a notebook that contains windows of various tools.  It is
+ * usually at the bottom of a window.
+ */
+class ToolsPane: public Notebook
+{
+};
+
+}
+
+#endif
