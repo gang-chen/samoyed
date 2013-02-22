dc49ab6d9a336e69692cda1945d632444919f67d
gang-chen <gang.chen.cn@gmail.com>
2013-02-22 09:21:54 +0800

    Update src/application/ui/bar.hpp

--- src/application/ui/bar.hpp
+++ src/application/ui/bar.hpp
@@ -5,30 +5,21 @@
 #define SMYD_BAR_HPP
 
 #include "widget.hpp"
-#include <map>
-#include <string>
-#include <boost/function.hpp>
 
 namespace Samoyed
 {
 
 /**
- * A bar is a named widget that can only be contained by a widget with bars.
+ * A bar is a widget that can only be contained by a widget with bars.
  */
 class Bar: public Widget
 {
 public:
-    typedef boost::function<Bar *(const char *name)> BarFactory;
-
     enum Orientation
     {
         ORIENTATION_HORIZONTAL = GTK_ORIENTATION_HORIZONTAL,
         ORIENTATION_VERTICAL = GTK_ORIENTATION_VERTICAL
     };
-
-    static bool registerBar(const char *name, const BarFactory &factory);
-
-    static bool unregisterBar(const char *name);
 
     Bar(const char *name): Widget(name) {}
 
@@ -36,9 +27,6 @@ public:
 
 protected:
     Bar(XmlElement &xmlElement): Widget(xmlElement) {}
-
-private:
-    static std::map<std::string, BarFactory> s_barRegistry;
 };
 
 }
