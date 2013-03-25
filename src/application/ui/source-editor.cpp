f10e442a349c9d2de575af683f73fd6f5d248358
gang-chen <gang.chen.cn@gmail.com>
2013-03-25 14:10:08 +0800

    Update source-editor.cpp

--- src/application/ui/source-editor.cpp
+++ src/application/ui/source-editor.cpp
@@ -6,7 +6,20 @@
 #endif
 #include "source-editor.hpp"
 #include "source-file.hpp"
+#include "../utilities/miscellaneous.hpp"
+#include <stdlib.h>
+#include <string.h>
+#include <list>
+#include <map>
+#include <string>
+#include <boost/any.hpp>
+#include <glib.h>
+#include <glib/gi18n-lib.h>
 #include <gtk/gtk.h>
+#include <libxml/tree.h>
+
+#define TEXT_EDITOR "text-editor"
+#define SOURCE_EDITOR "source-editor"
 
 namespace Samoyed
 {
@@ -21,6 +34,130 @@ void SourceEditor::createSharedData()
 void SourceEditor::destroySharedData()
 {
     g_object_unref(s_sharedTagTable);
+}
+
+bool SourceEditor::XmlElement::registerReader()
+{
+    return Widget::XmlElement::registerReader(SOURCE_EDITOR,
+                                              Widget::XmlElement::Reader(read));
+}
+
+bool SourceEditor::XmlElement::readInternally(xmlDocPtr doc,
+                                              xmlNodePtr node,
+                                              std::list<std::string> &errors)
+{
+    char *value, *cp;
+    bool textEditorSeen = false;
+    for (xmlNodePtr child = node->children; child; child = child->next)
+    {
+        if (strcmp(reinterpret_cast<const char *>(child->name),
+                   TEXT_EDITOR) == 0)
+        {
+            if (textEditorSeen)
+            {
+                cp = g_strdup_printf(
+                    _("Line %d: More than one \"%s\" elements seen.\n"),
+                    child->line, TEXT_EDITOR);
+                errors.push_back(cp);
+                g_free(cp);
+                return false;
+            }
+            if (!TextEditor::XmlElement::readInternally(doc, child, errors))
+                return false;
+            textEditorSeen = true;
+        }
+    }
+
+    if (!textEditorSeen)
+    {
+        cp = g_strdup_printf(
+            _("Line %d: \"%s\" element missing.\n"),
+            node->line, TEXT_EDITOR);
+        errors.push_back(cp);
+        g_free(cp);
+        return false;
+    }
+
+    // Verify that the file is a C/C++ source file.
+    char *type = getFileType(uri());
+    if (strcmp(type, "text/x-csrc") && strcmp(type, "text/x-chdr") &&
+        strcmp(type, "text/x-c++src") && strcmp(type, "text/x-c++hdr"))
+    {
+        g_free(type);
+        cp = g_strdup_printf(
+            _("Line %d: File \"%s\" is not a C/C++ source file.\n"),
+            node->line, uri());
+        errors.push_back(cp);
+        g_free(cp);
+        return false;
+    }
+    g_free(type);
+    return true;
+}
+
+xmlNodePtr SourceEditor::XmlElement::write() const
+{
+    char *cp;
+    xmlNodePtr node =
+        xmlNewNode(NULL,
+                   reinterpret_cast<const xmlChar *>(SOURCE_EDITOR));
+    xmlAddChild(node, TextEditor::XmlElement::write());
+    return node;
+}
+
+SourceEditor::XmlElement::XmlElement(const SourceEditor &editor):
+    TextEditor::XmlElement(editor)
+{
+}
+
+Editor *SourceEditor::XmlElement::restoreEditor(
+    std::map<std::string, boost::any> &options)
+{
+    return TextEditor::XmlElement::restoreEditor(options);
+}
+
+Widget *SourceEditor::XmlElement::restoreWidget()
+{
+    std::map<std::string, boost::any> options;
+    Editor *editor = restoreEditor(options);
+    if (!editor)
+        return NULL;
+    if (!static_cast<SourceEditor *>(editor)->restore())
+    {
+        editor->close();
+        return NULL;
+    }
+    return editor;
+}
+
+bool SourceEditor::setup()
+{
+    if (!TextEditor::setup(s_sharedTagTable))
+        return false;
+    return true;
+}
+
+SourceEditor *SourceEditor::create(TextFile &file, Project *project)
+{
+    SourceEditor *editor = new SourceEditor(file, project);
+    if (!editor->setup())
+    {
+        editor->close();
+        return NULL;
+    }
+    return editor;
+}
+
+bool SourceEditor::restore(XmlElement &xmlElement)
+{
+    if (!TextEditor::restore(xmlElement, s_sharedTagTable))
+        return false;
+    return true;
+}
+
+Widget::XmlElement *SourceEditor::save() const
+{
+    return new SourceEditor::XmlElement(*this);
 }
 
 }
