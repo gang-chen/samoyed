eb700fb6d5b2570f84f1927473968034b5afbd6f
gang-chen <gang.chen.cn@gmail.com>
2013-04-23 22:19:37 +0800

    Update plugin-manager.cpp

--- src/application/utilities/plugin-manager.cpp
+++ src/application/utilities/plugin-manager.cpp
@@ -2,11 +2,25 @@
 // Copyright (C) 2013 Gang Chen.
 
 #ifdef HAVE_CONFIG_H
-#include <config.h>
+# include <config.h>
 #endif
 #include "plugin-manager.hpp"
 
+namespace
+{
+
+const int CACHE_SIZE = 4;
+
+}
+
 namespace Samoyed
 {
+
+PluginManager::PluginManager():
+    m_nCachedPlugins(0),
+    m_lruCachedPlugin(NULL),
+    m_mruCachedPlugin(NULL)
+{
+}
 
 }
